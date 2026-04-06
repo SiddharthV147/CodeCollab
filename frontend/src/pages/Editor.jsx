8b180ad4adcca3487c83f5dea2f11ee84f26acd4
SiddharthV147 <siddharthvarade147@gmail.com>
2026-04-06 23:42:08 +0530

    Insert Operation setup done

--- frontend/src/pages/Editor.jsx
+++ frontend/src/pages/Editor.jsx
@@ -1,27 +1,16 @@
 import React from "react";
 import MonacoEditorComponent from "../components/MonacoEditorComponent";
-import { Panel, PanelGroup, ResizeHandle } from "react-resizable-panels";
 
 function Editor() {
     return (
-        <div className="h-screen w-full">
-            <PanelGroup direction="horizontal">
-                {/* Sidebar Panel */}
-                <Panel defaultSize={20} minSize={10} maxSize={40}>
-                    <div className="h-full bg-gray-100 p-2">
-                        Files
-                    </div>
-                </Panel>
-
-                <ResizeHandle className="w-2 bg-gray-300 hover:bg-blue-500 cursor-col-resize transition-colors" />
-
-                <Panel defaultSize={80}>
-                    <div className="h-full">
-                        <MonacoEditorComponent />
-                    </div>
-                </Panel>
-            </PanelGroup>
-        </div>
+        <>
+            <div className="h-screen w-screen">
+                {/* <div>Files</div> */}
+                <div className="h-full w-full">
+                    <MonacoEditorComponent />
+                </div>
+            </div>
+        </>
     );
 }
 

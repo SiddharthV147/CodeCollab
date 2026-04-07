e58fbe96839bee0bd60fb5b6fe0b441091c7ae31
SiddharthV147 <siddharthvarade147@gmail.com>
2026-04-07 15:27:45 +0530

    Insert operation done

--- backend/otengine/src/main/java/com/otengine/DocumentState.java
+++ backend/otengine/src/main/java/com/otengine/DocumentState.java
@@ -13,13 +13,13 @@ public class DocumentState {
     public void apply(Operation op) {
         if(op instanceof InsertOperation) {
 //            System.out.println("Applying Operation : "+op+" \nto doc :"+document);
-//            if(op.index() >= document.length()) {
-//                document.append(((InsertOperation) op).word());
-//            } else {
-//                this.document.insert(op.index(), ((InsertOperation) op).word());
-//            }
+            if(op.index() >= document.length()) {
+                document.append(((InsertOperation) op).word());
+            } else {
+                this.document.insert(op.index(), ((InsertOperation) op).word());
+            }
 
-            document.insert(op.index(), ((InsertOperation) op).word());
+//            document.insert(op.index(), ((InsertOperation) op).word());
         }
     }
 
--- backend/otengine/src/test/java/com/otengine/InsertTests.java
+++ backend/otengine/src/test/java/com/otengine/InsertTests.java
@@ -99,4 +99,140 @@ public class InsertTests {
 
         assertEquals("hello XYZZZworld", ds.getDocument());
     }
+
+    @Test
+    public void test7() {
+        DocumentState ds = new DocumentState("");
+        List<Operation> prev = new ArrayList<>();
+
+        InsertOperation op1 = new InsertOperation(1, 1, 1, 0, "ABC");
+        InsertOperation edited1 = (InsertOperation) otEngine.transform(prev, op1);
+        ds.apply(edited1);
+        prev.add(edited1);
+
+        InsertOperation op2 = new InsertOperation(1, 2, 1, 0, "XYZ");
+        InsertOperation edited2 = (InsertOperation) otEngine.transform(prev, op2);
+        ds.apply(edited2);
+
+        assertEquals("ABCXYZ", ds.getDocument());
+    }
+
+    @Test
+    public void test8() {
+        DocumentState ds = new DocumentState("hello");
+        List<Operation> prev = new ArrayList<>();
+
+        InsertOperation op1 = new InsertOperation(1, 1, 1, 5, " world");
+        InsertOperation edited1 = (InsertOperation) otEngine.transform(prev, op1);
+        ds.apply(edited1);
+        prev.add(edited1);
+
+        InsertOperation op2 = new InsertOperation(1, 2, 1, 5, "!!!");
+        InsertOperation edited2 = (InsertOperation) otEngine.transform(prev, op2);
+        ds.apply(edited2);
+
+        assertEquals("hello world!!!", ds.getDocument());
+    }
+
+    @Test
+    public void test9() {
+        DocumentState ds = new DocumentState("");
+        List<Operation> prev = new ArrayList<>();
+
+        InsertOperation op1 = new InsertOperation(1, 1, 1, 5, "X");
+        InsertOperation edited1 = (InsertOperation) otEngine.transform(prev, op1);
+        ds.apply(edited1);
+        prev.add(edited1);
+
+        InsertOperation op2 = new InsertOperation(1, 1, 1, 5, "Y");
+        InsertOperation edited2 = (InsertOperation) otEngine.transform(prev, op2);
+        ds.apply(edited2);
+
+        assertEquals("XY", ds.getDocument());
+    }
+
+    @Test
+    public void test10() {
+        DocumentState ds = new DocumentState("");
+        List<Operation> prev = new ArrayList<>();
+
+        InsertOperation op1 = new InsertOperation(1, 1, 1, 3, "");
+        InsertOperation edited1 = (InsertOperation) otEngine.transform(prev, op1);
+        ds.apply(edited1);
+        prev.add(edited1);
+
+        InsertOperation op2 = new InsertOperation(1, 2, 1, 3, "X");
+        InsertOperation edited2 = (InsertOperation) otEngine.transform(prev, op2);
+        ds.apply(edited2);
+
+        assertEquals("X", ds.getDocument());
+    }
+
+    @Test
+    public void test11() {
+        DocumentState ds = new DocumentState("");
+        List<Operation> history = new ArrayList<>();
+        List<Operation> transformedOps = new ArrayList<>();
+
+        InsertOperation op1 = new InsertOperation(0, 0, 0, 0, "h");
+        InsertOperation t1 = (InsertOperation) otEngine.transform(history, op1);
+        history.add(t1);
+        transformedOps.add(t1);
+
+        InsertOperation op2 = new InsertOperation(1, 1, 1, 1, "e");
+        InsertOperation t2 = (InsertOperation) otEngine.transform(history, op2);
+        history.add(t2);
+        transformedOps.add(t2);
+
+        InsertOperation op3 = new InsertOperation(2, 2, 2, 2, "l");
+        InsertOperation t3 = (InsertOperation) otEngine.transform(history, op3);
+        history.add(t3);
+        transformedOps.add(t3);
+
+        InsertOperation op4 = new InsertOperation(3, 3, 3, 3, "l");
+        InsertOperation t4 = (InsertOperation) otEngine.transform(history, op4);
+        history.add(t4);
+        transformedOps.add(t4);
+
+        InsertOperation op5 = new InsertOperation(4, 4, 4, 4, "o");
+        InsertOperation t5 = (InsertOperation) otEngine.transform(history, op5);
+        history.add(t5);
+        transformedOps.add(t5);
+
+        InsertOperation op6 = new InsertOperation(0, 0, 0, 5, " ");
+        InsertOperation t6 = (InsertOperation) otEngine.transform(history, op6);
+        history.add(t6);
+        transformedOps.add(t6);
+
+        InsertOperation op7 = new InsertOperation(1, 0, 0, 6, "w");
+        InsertOperation t7 = (InsertOperation) otEngine.transform(history, op7);
+        history.add(t7);
+        transformedOps.add(t7);
+
+        InsertOperation op8 = new InsertOperation(2, 0, 0, 7, "o");
+        InsertOperation t8 = (InsertOperation) otEngine.transform(history, op8);
+        history.add(t8);
+        transformedOps.add(t8);
+
+        InsertOperation op9 = new InsertOperation(3, 0, 0, 8, "r");
+        InsertOperation t9 = (InsertOperation) otEngine.transform(history, op9);
+        history.add(t9);
+        transformedOps.add(t9);
+
+        InsertOperation op10 = new InsertOperation(4, 0, 0, 9, "l");
+        InsertOperation t10 = (InsertOperation) otEngine.transform(history, op10);
+        history.add(t10);
+        transformedOps.add(t10);
+
+        InsertOperation op11 = new InsertOperation(5, 0, 0, 10, "d");
+        InsertOperation t11 = (InsertOperation) otEngine.transform(history, op11);
+        history.add(t11);
+        transformedOps.add(t11);
+
+        for (Operation op : transformedOps) {
+            ds.apply(op);
+        }
+
+        assertEquals("hello world", ds.getDocument());
+    }
 }
